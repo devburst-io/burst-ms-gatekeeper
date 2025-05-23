e7d687f3c170d0ccb2e33ab1c93b67e7f3d5def1
Rafael Karczevski <rkarczevski@gmail.com>
2025-05-22 20:09:33 -0400

    feat: adicionar validação de campo obrigatório para nome nos DTOs de criação e atualização de organização

--- src/organization/dto/create-organization.dto.ts
+++ src/organization/dto/create-organization.dto.ts
@@ -1,8 +1,9 @@
-import { IsString, MinLength } from "class-validator";
+import { IsNotEmpty, IsString, MinLength } from "class-validator";
 
 export class CreateOrganizationDto {
     @IsString()
     @MinLength(3)
+    @IsNotEmpty()
     name: string;
 
     @IsString()
--- src/organization/dto/update-organization.dto.ts
+++ src/organization/dto/update-organization.dto.ts
@@ -1,8 +1,9 @@
-import { IsOptional, IsString, IsUrl } from "class-validator";
+import { IsNotEmpty, IsOptional, IsString, IsUrl } from "class-validator";
 
 export class UpdateOrganizationDto {
   @IsOptional()
   @IsString()
+  @IsNotEmpty()
   name?: string;
 
   @IsOptional()
